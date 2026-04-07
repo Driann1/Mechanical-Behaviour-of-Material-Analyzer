d8d121a8c75a819cb25011be1ee2914a099bf4e0
Drian <adriamuhamad1@gmail.com>
2026-04-07 23:17:50 +0700

    version 1

---
+++ stress-strain.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+
+# === 1. Baca file CSV ===
+
+pilih_material = input("Material apa yang ingin di pilih \n")
+
+file_path_analisis = f"hasil_analisis/{pilih_material}"
+file_path_analisis2 = f"{file_path_analisis}/{pilih_material}.txt"
+file_path_curve = f"{file_path_analisis}/{pilih_material}.png"
+file_path_data = f"data_stress_strain/{pilih_material}.csv"
+
+
+Path(file_path_analisis).mkdir(parents=True, exist_ok=True)
+data = pd.read_csv(file_path_data)
+
+# === 2. Ambil kolom ===
+strain = data['strain']
+stress = data['stress']
+
+# === 3. Plot grafik ===
+plt.figure()
+plt.plot(strain, stress)
+
+# === 4. Label & judul ===
+plt.xlabel('Strain')
+plt.ylabel('Stress (MPa)')
+plt.title('Stress-Strain Curve')
+
+# === 5. Grid biar enak dibaca ===
+plt.grid()
+plt.yticks(np.arange(0, 600, 50))      # tiap 50 MPa
+plt.xticks(np.arange(0, 0.2, 0.02))    # tiap 0.02 strain
+
+# === 6. Tampilkan grafik ===
+uts = stress.max()
+uts_index = stress.idxmax()
+uts_strain = strain[uts_index]
+
+plt.savefig(file_path_curve)
+
+
+#Analisis
+with open(file_path_analisis2, "w") as f:
+    f.write("=== HASIL ANALISIS MATERIAL ===\n\n")
+    
+    f.write(f"UTS (Ultimate Tensile Strength): {uts} MPa\n")
+    f.write(f"Strain saat UTS: {uts_strain}\n\n")
+plt.show()
